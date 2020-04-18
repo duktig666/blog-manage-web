cc7afb1aea262de1f9a356ca082e98b347f34425
rsw <1487660836@qq.com>
2020-04-18 22:08:11 +0800

    环境搭建 rsw

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    // import __ from '__' // __是需要手动引入的文件
+    export default {
+        name: '',
+        components: {},
+        data() {
+            return {msg: '这个是Home模板页'}
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="postcss">
+    body {
+        background-color: #ff0000;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
