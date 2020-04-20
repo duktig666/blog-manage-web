425b37efdf20372bbabb35ba6932b14c719a3cbb
qianjin <1542890862@qq.com>
2020-04-20 21:36:55 +0800

    登录界面未完成 JQJ 2020.04.20 21:36

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,77 +1,102 @@
 <template>
     <v-app>
         <v-content>
-            <v-form v-model="valid">
-                <v-container>
-                    <v-row>
-                        <v-col
-                                cols="12"
-                                md="4"
-                        >
-                            <v-text-field
-                                    v-model="firstname"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="First name"
-                                    required
-                            />
-                        </v-col>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <v-card class="elevation-12">
+                            <v-toolbar dark color="primary">
+                                <v-toolbar-titletitle>个人博客后台管理</v-toolbar-titletitle>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                            <v-card-text>
+                                <!-- 表单   -->
+                                <v-form
+                                        ref="form"
+                                        v-model="valid"
+                                        lazy-validation>
+                                    <v-text-field prepend-icon="person"  v-model="username" label="用户名" type="text"/>
+                                    <v-text-field
+                                            ref="password"
+                                            prepend-icon="lock"
+                                            v-model="password"
+                                            label="密码"
+                                            id="password"
+                                            :append-icon="e1 ? 'visibility' : 'visibility_off'"
+                                            :append-icon-cb="() => (e1 = !e1)"
+                                            :type="e1 ? 'text' : 'password'"
+                                    ></v-text-field>
+                                        <v-row no-gutters>
+                                            <v-col cols="8">
+                                                <v-text-field
+                                                    prepend-icon="code"
+                                                    v-model="code"
+                                                    label="验证码"
+                                                    type="text">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="4">
+                                                <v-container fluid fill-height>
+                                                    <v-layout align-center justify-center>
+                                                    <img src="../assets/code.png"/>
+                                                    </v-layout>
+                                                </v-container>
+                                            </v-col>
+                                        </v-row>
+<!--                                    <v-content class="d-inline-block">-->
 
-                        <v-col
-                                cols="12"
-                                md="4"
-                        >
-                            <v-text-field
-                                    v-model="lastname"
-                                    :rules="nameRules"
-                                    :counter="10"
-                                    label="Last name"
-                                    required
-                            />
-                        </v-col>
 
-                        <v-col
-                                cols="12"
-                                md="4"
-                        >
-                            <v-text-field
-                                    v-model="email"
-                                    :rules="emailRules"
-                                    label="E-mail"
-                                    required
-                            />
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-form>
+<!--                                    </v-content>-->
+<!--                                    <v-content class="d-inline-block ">-->
+<!--                                        <img src="../assets/code.png"/>-->
+<!--                                    </v-content>-->
+
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                        color="error"
+                                        class="mr-4"
+                                        @click="reset"
+                                >
+                                    重置
+                                </v-btn>
+                                <v-btn color="primary" @click="doLogin">登录</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
         </v-content>
+        <v-dialog v-model="dialog" width="300px">
+            <v-alert icon="warning" color="error" :value="true">
+                用户名和密码不能为空
+            </v-alert>
+        </v-dialog>
     </v-app>
-
 </template>
 
 <script>
-    // import __ from '__' // __是需要手动引入的文件
     export default {
-        name: 'Login',
-        components: {},
         data: () => ({
-            valid: false,
-            firstname: '',
-            lastname: '',
-            nameRules: [
-                v => !!v || 'Name is required',
-                v => v.length <= 10 || 'Name must be less than 10 characters',
-            ],
-            email: '',
-            emailRules: [
-                v => !!v || 'E-mail is required',
-                v => /.+@.+/.test(v) || 'E-mail must be valid',
-            ],
+            username: "",
+            password: "",
+            dialog: false,
+            e1:false
         }),
-        methods: {}
-    }
+        methods: {
+            reset () {
+                this.$refs.form.reset()
+            },
+            doLogin() {
+                if (!this.username || !this.password) {
+                    this.dialog = true;
+                    return false;
+                }
+                console.log(this.username + " ... " + this.password);
+                this.$router.push("/");
+            }
+        }
+    };
 </script>
-
-<style lang="postcss" scoped>
-
-</style>
